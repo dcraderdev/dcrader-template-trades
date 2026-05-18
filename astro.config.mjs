bf358e4426f5cf5d161c2eef0f8a3d4acbbe3ffe
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2026-05-17 17:04:43 -0700

    add sitemap + robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dcrader-template-trades.vercel.app',
   output: 'static',
   compressHTML: true,
   prefetch: { prefetchAll: true, defaultStrategy: 'viewport' },
   build: { inlineStylesheets: 'auto' },
-  integrations: [tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
